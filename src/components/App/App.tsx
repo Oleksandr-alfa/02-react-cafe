0cd19a3b4e73a5254dd72d30823b6a2ee3534826
Oleksandr-alfa <a_kos_76_@ukr.net>
2026-07-03 23:43:15 +0300

    create cafe app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,13 @@
 
 import css from './App.module.css';
 
-import CafeInfo from './components/CafeInfo/CafeInfo.tsx';
-import VoteOptions from './components/VoteOptions/VoteOptions.tsx';
-import VoteStats from './components/VoteStats/VoteStats.tsx';
-import Notification from './components/Notification/Notification.tsx';
+import CafeInfo from '../CafeInfo/CafeInfo.tsx';
+import VoteOptions from '../VoteOptions/VoteOptions.tsx';
+import VoteStats from '../VoteStats/VoteStats.tsx';
+import Notification from '../Notification/Notification.tsx';
 import { useState } from "react";
-import type { Votes } from './types/votes.ts';
-import type { VoteType } from './types/votes.ts';  
+import type { Votes } from '../../types/votes.ts';
+import type { VoteType } from '../../types/votes.ts';  
 
 
 
